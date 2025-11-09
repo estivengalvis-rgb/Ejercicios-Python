927f8931c30ee72a443f42eedb528aaf9cbbefd2
estivengalvis-rgb <galvisstivens@gmail.com>
2025-11-08 19:45:01 -0500

    Add files via upload

---
+++ 39. IntervaloYo.py
@@ -0,0 +1,7 @@
+a=float(input("Ingrese el primer intervalo "))
+b=float(input("Ingrese el segundo intervalo "))
+c=float(input("Ingrese un número "))
+if c>=a and c<=b:
+    print("El número está dentro del intervalo")
+else:
+    print("El número no está dentro del intervalo")
